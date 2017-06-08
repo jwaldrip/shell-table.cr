5f37ebc1dfe88a06708cd2314804a866ac226404
jwaldrip <jason@waldrip.net>
2017-06-08 14:15:51 -0600

    add a dsl example

---
+++ examples/dsl.cr
@@ -0,0 +1,12 @@
+require "../src/shell-table"
+
+table = ShellTable.new
+table.labels = ["Col1", "Col2", "Col3", "Col4", "Col5"]
+10.times do |r|
+  row = table.add_row
+  5.times do |c|
+    row.add_column "Row ##{r + 1}, Col ##{c + 1}"
+  end
+end
+
+puts table
